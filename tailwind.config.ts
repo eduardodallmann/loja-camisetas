2e4a6f8c1a32d7088ec7d1d0a91d408ede16920a
Eduardo Dallmann <eduardodallmann@gmail.com>
2024-04-03 14:59:02 -0300

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,9 +7,21 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class',
+  safelist: [
+    'bg-red-600',
+    'bg-blue-600',
+    'bg-green-600',
+    'bg-yellow-600',
+    'bg-black',
+    'bg-white',
+    'bg-pink-600',
+    'border-gray-300',
+    'border',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
